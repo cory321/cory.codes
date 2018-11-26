e233330d5c1c78dca79f26a083030636a6462709
cory321 <cory321@gmail.com>
2018-11-25 23:22:57 -0800

    Header logo, navigation, and hero message styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+// import '../node_modules/devicons/css/devicons.min.css'
+import './sass/bootstrap-grid.min.css'
+import './sass/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*ReactDOM.render(<App />, document.getElementById('root'));*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
